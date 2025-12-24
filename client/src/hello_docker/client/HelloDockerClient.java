9bf507abe9ef923493c1aff1e4dbd15aa88b9de2
Sergei Aidinov <sergey.aydinov@gmail.com>
2025-12-24 23:29:43 +0400

    made simple client

--- client/src/hello_docker/client/HelloDockerClient.java
+++ client/src/hello_docker/client/HelloDockerClient.java
@@ -1,32 +1,37 @@
 package hello_docker.client;
 
 import java.net.http.*;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.time.Duration;
 import java.util.Objects;
+import java.util.Random;
 
 public class HelloDockerClient {
 
-	public static void main(String[] args)  {
-		/*HttpClient client = HttpClient.newHttpClient();
+	public static void main(String[] args) throws InterruptedException, IOException {
+		HttpClient client = HttpClient.newHttpClient();
 
 		while (true) {
 			HttpRequest request = null;
 			HttpResponse<String> response = null;
+			String id = String.valueOf((int) (Math.random() * 10));
+			System.out.println("Request for ID: " + id);
 			try {
-				request = HttpRequest.newBuilder().uri(URI.create("http://volumes-java-server:8080"))
+				request = HttpRequest.newBuilder().uri(URI.create("http://volumes-java-server:8080/" + id))
 						.timeout(Duration.ofSeconds(5)).GET().build();
 				response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
 			} catch (Exception e) {
 				System.out.println("Server did not respond");
+				System.out.println(e.getMessage());
 			}
 			if (Objects.nonNull(response))
 				System.out.println("Response: " + response.body());
 			Thread.sleep(2000);
 		}
-	}*/
+	}
 
-}
 }
